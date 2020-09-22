fba3f0a21d0d4d74b4acf16c2cbfa3dab7939f5b
Izabella Hermanowicz <izabella.hermanowicz@gmail.com>
2020-09-22 14:01:28 +0200

    Partly working version of second exercise

--- zad2.py
+++ zad2.py
@@ -4,11 +4,14 @@ with open('12.json') as f:
   data_json = json.load(f)
 
 def getItems(X, dict):
+    obj={}
    
     dict=dict['data']
     for x in dict:
         if x.get('amount') < X:
-            print(x)
+            obj.update(x)
 
+    new_json= json.dumps(obj)
+    print(new_json)
 
 getItems(5, data_json)
