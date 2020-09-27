c093081c48b6c57befe070a11cb62745e6a7978b
Izabella Hermanowicz <izabella.hermanowicz@gmail.com>
2020-09-27 18:20:42 +0200

    Exercise 4 partly made

--- zad4.py
+++ zad4.py
@@ -2,6 +2,7 @@ from typing import Optional
 from fastapi import FastAPI
 import csv
 import json
+from datetime import datetime, date, time, timezone
 
 json_data = [json.dumps(d) for d in csv.DictReader(open('tags.csv'))]
 
@@ -12,5 +13,9 @@ def read_root():
     return {"Hello": "World"}
 
 @app.get("/api/scan")
+def scan(data:json_data):
+    return {"request":data}
 
 @app.post("/api/results")
+def results(Antenna_ID:int, Frequency:int, EPC_96:complex, Peak_RSSI:int, Phase:int, RF_Doppler_Frequency:int, Tags_Count:int, Seen:datetime):
+    
